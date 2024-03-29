ea2bbe69ded0a2d5b479ca66ed15949308699738
freemabd <brian.d.freeman@gmail.com>
2024-03-29 12:09:34 -0500

    adding initial implementation

--- src/main/java/vumc/org/springreact/controller/BourbonDistilleryController.java
+++ src/main/java/vumc/org/springreact/controller/BourbonDistilleryController.java
@@ -1,10 +1,25 @@
 package vumc.org.springreact.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vumc.org.springreact.model.BourbonDistillery;
+import vumc.org.springreact.repository.BourbonDistilleryRepository;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api")
 public class BourbonDistilleryController {
 
+    private final BourbonDistilleryRepository bourbonDistilleryRepository;
+
+    public BourbonDistilleryController(BourbonDistilleryRepository bourbonDistilleryRepository) {
+        this.bourbonDistilleryRepository = bourbonDistilleryRepository;
+    }
+
+    @GetMapping("/distilleries")
+    Collection<BourbonDistillery> distilleries() {
+        return bourbonDistilleryRepository.findAll();
+    }
 }
